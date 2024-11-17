81b9ee2312471c9ae1a8ccd6b2687b75a0857c1d
Loyola Leo Martin <masterdjmov@gmail.com>
2024-11-17 00:32:16 -0300

    modificando auth entidad, le agregue para que sea la tabla login

--- src/modules/auth/entities/auth.entity.ts
+++ src/modules/auth/entities/auth.entity.ts
@@ -1,6 +1,22 @@
-import { Entity } from "typeorm";
+import { Column, CreateDateColumn, Entity, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
 
 @Entity()
 export class Auth {
+    @PrimaryGeneratedColumn('increment')
+    id: number;
 
+    @Column()
+    username: string;
+
+    @Column()
+    password: string;
+
+    @Column()
+    usuario_id: number;
+
+    @CreateDateColumn({type: 'timestamp'})  //automatico
+    created_at: Date;
+
+    @UpdateDateColumn({type: 'timestamp'})  //automatico
+    updated_at: Date;
 }
