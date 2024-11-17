c25e10495881f8aabe4f83a6f03a85617323f238
Loyola Leo Martin <masterdjmov@gmail.com>
2024-11-17 00:06:40 -0300

    creando entidad de informe

--- src/modules/informes/entities/informe.entity.ts
+++ src/modules/informes/entities/informe.entity.ts
@@ -1,1 +1,25 @@
-export class Informe {}
+import { Column, CreateDateColumn, Entity, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
+
+@Entity()
+export class Informe {
+    @PrimaryGeneratedColumn('increment')
+    id: number;
+
+    @Column()
+    usuario_id: number;
+
+    @Column()
+    fecha_desde: Date;
+
+    @Column()
+    fecha_hasta: Date;
+
+    @Column()
+    filtro: string;
+
+    @CreateDateColumn({type: 'timestamp'})  //automatico
+    created_at: Date;
+
+    @UpdateDateColumn({type: 'timestamp'})  //automatico
+    updated_at: Date;
+}
