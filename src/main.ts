0475c5f512fe483c8313457b8bf107c32a85a5f1
Loyola Leo Martin <masterdjmov@gmail.com>
2024-11-16 21:35:38 -0300

    creando la entidad de usuario con sus campos

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  /*Agrego swagger */
+  const config = new DocumentBuilder()
+    .addBearerAuth()
+    .setTitle('items Documentation')
+    .setDescription('Aquí prueba de una descripción')
+    .setVersion('1.0')
+    .addTag('items')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('documentation', app, documentFactory);
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
--- src/modules/usuarios/entities/usuario.entity.ts
+++ src/modules/usuarios/entities/usuario.entity.ts
@@ -1,1 +1,29 @@
-export class Usuario {}
+import { Column, CreateDateColumn, Entity, PrimaryGeneratedColumn, Timestamp, UpdateDateColumn } from "typeorm";
+
+@Entity()
+export class Usuario {
+    @PrimaryGeneratedColumn('increment')
+    id: number;
+
+    @Column()
+    nombre: string;
+
+    @Column()
+    apellido: string;
+
+    @Column()
+    email: string;
+
+    @Column()
+    password: string;
+
+    @Column({nullable: true})   //permito null
+    foto: string
+
+    @CreateDateColumn({type: 'timestamp'})  //automatico
+    created_at: Date;
+
+    @UpdateDateColumn({type: 'timestamp'})  //automatico
+    updated_at: Date;
+
+}
