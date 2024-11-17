5c29fe577af8d41a2b557c7418e14d0ca4713290
Loyola Leo Martin <masterdjmov@gmail.com>
2024-11-17 00:23:34 -0300

    incorporando los import para leer el typeorm en cada modulo

--- src/modules/gastos/gastos.module.ts
+++ src/modules/gastos/gastos.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { GastosService } from './gastos.service';
 import { GastosController } from './gastos.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Gasto } from './entities/gasto.entity';
 
 @Module({
+  imports:[TypeOrmModule.forFeature([Gasto])],
   controllers: [GastosController],
   providers: [GastosService],
 })
--- src/modules/informes/informes.module.ts
+++ src/modules/informes/informes.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { InformesService } from './informes.service';
 import { InformesController } from './informes.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Informe } from './entities/informe.entity';
 
 @Module({
+  imports:[TypeOrmModule.forFeature([Informe])],
   controllers: [InformesController],
   providers: [InformesService],
 })
