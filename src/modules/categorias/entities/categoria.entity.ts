77f37de2f503e169e175e66fa4b5feba841f5875
Loyola Leo Martin <masterdjmov@gmail.com>
2024-11-17 00:03:16 -0300

    creando entiedad categoria

--- src/modules/categorias/entities/categoria.entity.ts
+++ src/modules/categorias/entities/categoria.entity.ts
@@ -1,1 +1,19 @@
-export class Categoria {}
+import { Column, CreateDateColumn, Entity, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
+
+@Entity()
+export class Categoria {
+    @PrimaryGeneratedColumn('increment')
+    id: number;
+
+    @Column()
+    nombre: string;
+
+    @Column({type: 'text'})
+    descripcion: string;
+
+    @CreateDateColumn({type: 'timestamp'})  //automatico
+    created_at: Date;
+
+    @UpdateDateColumn({type: 'timestamp'})  //automatico
+    updated_at: Date;
+}
