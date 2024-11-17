f817339cb52097a63675f27d9cf73acd7046b90d
Loyola Leo Martin <masterdjmov@gmail.com>
2024-11-17 00:18:11 -0300

    aplicando apitags a los controllers, para poder verlos en swagger

--- src/modules/informes/informes.controller.ts
+++ src/modules/informes/informes.controller.ts
@@ -2,7 +2,9 @@ import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/commo
 import { InformesService } from './informes.service';
 import { CreateInformeDto } from './dto/create-informe.dto';
 import { UpdateInformeDto } from './dto/update-informe.dto';
+import { ApiTags } from '@nestjs/swagger';
 
+@ApiTags('informes')
 @Controller('informes')
 export class InformesController {
   constructor(private readonly informesService: InformesService) {}
