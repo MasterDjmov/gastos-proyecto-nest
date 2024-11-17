d763f98d0090a505500ce64f62b67fafd4d8d550
Loyola Leo Martin <masterdjmov@gmail.com>
2024-11-16 21:42:17 -0300

    creando la entidad de gastos

--- src/modules/gastos/entities/gasto.entity.ts
+++ src/modules/gastos/entities/gasto.entity.ts
@@ -1,1 +1,25 @@
-export class Gasto {}
+import { Column, CreateDateColumn, Entity, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
+
+@Entity()
+export class Gasto {
+    @PrimaryGeneratedColumn('increment')
+    id: number;
+
+    @Column()
+    usuario_id: number;
+
+    @Column({type: 'text'})
+    descripcion: string;
+
+    @Column()
+    fecha: Date;
+
+    @Column()
+    categoria_id: number;
+
+    @CreateDateColumn({type: 'timestamp'})  //automatico
+    created_at: Date;
+
+    @UpdateDateColumn({type: 'timestamp'})  //automatico
+    updated_at: Date;
+}
