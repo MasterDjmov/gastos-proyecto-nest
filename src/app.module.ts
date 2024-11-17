00fea4e7af303aae4f6a997549e5579f4cabcec3
Loyola Leo Martin <masterdjmov@gmail.com>
2024-11-16 21:17:04 -0300

    creando los modulos del sistema

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './modules/auth/auth.module';
+import { UsuariosModule } from './modules/usuarios/usuarios.module';
+import { GastosModule } from './modules/gastos/gastos.module';
+import { CategoriasModule } from './modules/categorias/categorias.module';
+import { InformesModule } from './modules/informes/informes.module';
 
 @Module({
-  imports: [],
+  imports: [AuthModule, UsuariosModule, GastosModule, CategoriasModule, InformesModule],
   controllers: [AppController],
   providers: [AppService],
 })
